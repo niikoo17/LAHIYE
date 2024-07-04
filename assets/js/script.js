104fc05eb8f6c114119954c84e264f4647be69ba
niikoo17 <nihadna@code.edu.az>
2024-07-04 05:34:39 +0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let home = document.querySelector("header-links-home")
+let homeDiv = document.getElementById("header-links-home div")
+
+home.addEventListener("click" , function(){
+    document.querySelector("header-links-home div").style.display = "block";
+})
+
